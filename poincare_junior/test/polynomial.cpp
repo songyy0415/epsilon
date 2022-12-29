01545e53c14afcf29e8c787b303f6fe778ebe846
Emilie Feral <emilie.feral@numworks.com>
2022-12-29 10:46:54 +0100

    [poincare_junior] Add test on Polynomial::Multiplication

--- poincare_junior/test/polynomial.cpp
+++ poincare_junior/test/polynomial.cpp
@@ -5,7 +5,7 @@
 using namespace Poincare;
 
 void assert_polynomial_is_parsed(const Node node, const Node expectedVariables, const Node expectedPolynomial) {
-  CachePool::sharedCachePool()-> editionPool()->flush();
+  CachePool::sharedCachePool()->editionPool()->flush();
   EditionReference variables = PolynomialParser::GetVariables(node);
   assert_trees_are_equal(variables, expectedVariables);
   EditionReference ref(node);
@@ -38,11 +38,19 @@ void testPolynomialParsing() {
 
 void testPolynomialOperations() {
   /* A = x^2 + 3*x*y + y + 1 */
-  EditionReference polA = EditionReference(Pol({2, 1, 0}, Symb("x"), 1_sn, Pol({1},  Symb("y"), 3_n), Pol({1, 0}, Symb("y"), 1_sn, 1_sn)));
+  Node polA = Pol({2, 1, 0}, Symb("x"), 1_sn, Pol({1},  Symb("y"), 3_n), Pol({1, 0}, Symb("y"), 1_sn, 1_sn));
   /* B = x^3 + 2*x*y^2 + 7*x*y + 23 */
-  EditionReference polB = EditionReference(Pol({3, 1, 0}, Symb("x"), 1_sn, Pol({2, 1},  Symb("y"), 2_sn, 7_n), 23_n));
+  Node polB = Pol({3, 1, 0}, Symb("x"), 1_sn, Pol({2, 1},  Symb("y"), 2_sn, 7_n), 23_n);
 
   /* A + B = x^3 + x^2 + 2*x*y^2 + 10*x*y + y + 24 */
   // TODO once basicReduction implemented assert_trees_are_equal(Polynomial::Addition(polA, polB), EditionReference(Pol({3, 2, 1, 0}, Symb("x"), 1_sn, 1_sn, Pol({2, 1}, Symb("y"), 2_sn, 10_n), Pol({1, 0}, Symb("y"), 1_sn, 24_n))));
-  assert_trees_are_equal(Polynomial::Addition(polA, polB), EditionReference(Pol({3, 2, 1, 0}, Symb("x"), 1_sn, 1_sn, Pol({2, 1}, Symb("y"), 2_sn, Add(3_n, 7_n)), Pol({1, 0}, Symb("y"), 1_sn, Add(1_sn, 23_n)))));
+  assert_trees_are_equal(Polynomial::Addition(EditionReference(polA), EditionReference(polB)), EditionReference(Pol({3, 2, 1, 0}, Symb("x"), 1_sn, 1_sn, Pol({2, 1}, Symb("y"), 2_sn, Add(3_n, 7_n)), Pol({1, 0}, Symb("y"), 1_sn, Add(1_sn, 23_n)))));
+  CachePool::sharedCachePool()->editionPool()->flush();
+
+  /* A * B = x^5 + 3yx^4 + (2y^2+8y+1)*x^3 + (6y^3+21y^2+23)x^2 + (2y^3+9y^2+ 76y)x + 23y + 23 */
+  // TODO once basicReduction assert_trees_are_equal(Polynomial::Multiplication(EditionReference(polA), EditionReference(polB)), EditionReference(Pol({5, 4, 3, 2, 1, 0}, Symb("x"), 1_sn, Pol({1}, Symb("y"), 3_n), Pol({2, 1, 0}, Symb("y"), 2_sn, 8_n, 1_sn), Pol({3, 2, 0}, Symb("y"), 6_n, 21_n, 23_n), Pol({3, 2, 1}, Symb("y"), 2_sn, 9_n, 76_n, Pol({1, 0}, Symb("y"), 23_n, 23_n))));
+  assert_trees_are_equal(Polynomial::Multiplication(EditionReference(polA), EditionReference(polB)), EditionReference(Pol({5, 4, 3, 2, 1, 0}, Symb("x"), Mult(1_sn, 1_sn), Pol({1}, Symb("y"), Mult(3_n, 1_sn)), Pol({2, 1, 0}, Symb("y"), Mult(2_sn, 1_sn), Add( Mult(1_sn, 1_sn), Mult(7_n, 1_sn)), Mult(1_sn, 1_sn)), Pol({3, 2, 0}, Symb("y"),  Mult(3_n, 2_sn), Mult(3_n, 7_n), Mult(1_sn, 23_n)), Pol({3, 2, 1}, Symb("y"), Mult(1_sn, 2_sn), Add(Mult(1_sn, 2_sn), Mult(1_sn, 7_n)), Add(Mult(1_sn, 7_n), Mult(3_n, 23_n))), Pol({1, 0}, Symb("y"), Mult(1_sn, 23_n), Mult(1_sn, 23_n)))));
+  CachePool::sharedCachePool()->editionPool()->flush();
+
+  /* */
 }
