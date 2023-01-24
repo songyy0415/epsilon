8e5a2d2d249dde1d5a631991b14e83c33766d5b7
MarcNumworks <marc.coudriau@numworks.com>
2023-01-24 11:00:33 +0100

    [poincare_junior/test] Fix match tests

--- poincare_junior/test/match.cpp
+++ poincare_junior/test/match.cpp
@@ -9,17 +9,17 @@ using namespace PoincareJ;
 void testPlaceholders() {
   using enum Placeholder;
   constexpr Tree a = A;
-  (void) Add(2_n, a);
-  (void) Add(2_n, Tree(A));
+  (void) Add("2"_n, a);
+  (void) Add("2"_n, Tree(A));
 }
 QUIZ_CASE(pcj_placeholders) { testPlaceholders(); }
 
 void testContext() {
   using enum Placeholder;
   Context ctx;
-  constexpr Tree t = Add(2_n, 1_n);
+  constexpr Tree t = Add("2"_n, "1"_n);
   ctx[A] = t;
-  constexpr Tree structure = Mult(5_n, Add(Tree(A), Tree(A)));
+  constexpr Tree structure = Mult("5"_n, Add(Tree(A), Tree(A)));
   EditionReference exp = ctx.build(structure);
   exp.log();
 }
@@ -27,11 +27,11 @@ QUIZ_CASE(pcj_context) { testContext(); }
 
 void testMatch() {
   using enum Placeholder;
-  constexpr Tree t = Add(2_n, 1_n);
+  constexpr Tree t = Add("2"_n, "1"_n);
   constexpr Tree p = A;
   Context ctx = Context::Match(p, t);
   ctx[A].log();
-  constexpr Tree p2 = Add(p, 1_n);
+  constexpr Tree p2 = Add(p, "1"_n);
   Context ctx2 = Context::Match(p2, t);
   ctx2[A].log();
 }
@@ -40,14 +40,14 @@ QUIZ_CASE(pcj_match) { testMatch(); }
 void testRewrite() {
   using enum Placeholder;
   constexpr Tree p = Add(Tree(A), Tree(A));
-  constexpr Tree s = Mult(2_n, Tree(A));
+  constexpr Tree s = Mult("2"_n, Tree(A));
   EditionReference ref = EditionReference::Push<BlockType::Addition>(2);
-  EditionReference::Push<BlockType::IntegerShort>(5);
-  EditionReference::Push<BlockType::IntegerShort>(5);
+  EditionReference::Push<BlockType::IntegerShort>(static_cast<int8_t>(5));
+  EditionReference::Push<BlockType::IntegerShort>(static_cast<int8_t>(5));
   EditionReference result = ref.rewrite(p, s);
 
   constexpr Tree a = Tree(A);
-  EditionReference result2 = ref.rewrite(Add(a, a), Mult(2_n, a));
+  EditionReference result2 = ref.rewrite(Add(a, a), Mult("2"_n, a));
   result2.log();
 }
 QUIZ_CASE(pcj_rewrite) { testRewrite(); }
