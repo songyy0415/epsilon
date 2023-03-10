e135f55c9c331d339a71d0a5778d2c2ce408aff8
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2023-03-10 15:41:54 +0100

    [poincare_junior] Improve VerticalOffsetLayout

--- poincare_junior/src/layout/vertical_offset_layout.cpp
+++ poincare_junior/src/layout/vertical_offset_layout.cpp
@@ -4,16 +4,37 @@
 namespace PoincareJ {
 
 KDSize VerticalOffsetLayout::Size(const Node node, KDFont::Size font) {
-  return Render::Size(node.childAtIndex(0), font);
+  assert(IsSuffixSuperscript(node));
+  KDSize indexSize = Render::Size(node.childAtIndex(0), font);
+  const Node base = BaseLayout(node);
+  KDCoordinate baseHeight = base.isUninitialized() ? KDFont::GlyphHeight(font) : Render::Size(base, font).height();
+  return KDSize(indexSize.width(), baseHeight - k_indiceHeight + indexSize.height());
 }
 
 KDCoordinate VerticalOffsetLayout::Baseline(const Node node, KDFont::Size font) {
-  // TODO : Use previous layout for accurate baseline
-  return Render::Size(node.childAtIndex(0), font).height();
+  assert(IsSuffixSuperscript(node));
+  const Node base = BaseLayout(node);
+  KDCoordinate baseBaseline = base.isUninitialized() ? KDFont::GlyphHeight(font) / 2 : Render::Baseline(base, font);
+  KDCoordinate indexHeight = Render::Size(node.childAtIndex(0), font).height();
+  return indexHeight - k_indiceHeight + baseBaseline;
 }
 
 KDPoint VerticalOffsetLayout::PositionOfChild(const Node node, int childIndex, KDFont::Size font) {
+  assert(IsSuffixSuperscript(node));
   return KDPointZero;
+}
+
+const Node VerticalOffsetLayout::BaseLayout(const Node node) {
+  const Node parent = node.parent();
+  if (parent.type() != BlockType::RackLayout) {
+    return Node();
+  }
+  assert(IsSuffixSuperscript(node));
+  const Node previousNode = node.previousTree();
+  if (previousNode == parent) {
+    return Node();
+  }
+  return previousNode;
 }
 
 }
--- poincare_junior/src/layout/vertical_offset_layout.h
+++ poincare_junior/src/layout/vertical_offset_layout.h
@@ -6,13 +6,17 @@
 
 namespace PoincareJ {
 
-/* This is a simplified VerticalOffsetLayout, only suffix superscript and
- * ignoring previous layouts. */
 class VerticalOffsetLayout {
 public:
   static KDSize Size(const Node node, KDFont::Size font);
   static KDCoordinate Baseline(const Node node, KDFont::Size font);
   static KDPoint PositionOfChild(const Node node, int childIndex, KDFont::Size font);
+  // TODO : Implement prefix and subscript logic
+  static bool IsSuffixSuperscript(const Node node) { return true; }
+private:
+  constexpr static KDCoordinate k_indiceHeight = 10;
+
+  static const Node BaseLayout(const Node node);
 };
 
 }
