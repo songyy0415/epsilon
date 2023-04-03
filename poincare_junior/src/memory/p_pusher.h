d4d7058792ce7c493cedeecb11dc74f1d684f2b2
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2023-04-03 15:25:04 +0200

    [poincare_junior] Fix _PP_NARG when no arguments

--- poincare_junior/src/memory/p_pusher.h
+++ poincare_junior/src/memory/p_pusher.h
@@ -4,9 +4,9 @@
 // Helper to count VA_ARGS
 // https://stackoverflow.com/questions/2124339/c-preprocessor-va-args-number-of-arguments
 
-#define _PP_NARG(...) _PP_NARG_(__VA_ARGS__, _PP_RSEQ_N())
+#define _PP_NARG(...) _PP_NARG_(_0, ## __VA_ARGS__, _PP_RSEQ_N())
 #define _PP_NARG_(...) _PP_ARG_N(__VA_ARGS__)
-#define _PP_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, N, ...) N
+#define _PP_ARG_N(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, N, ...) N
 #define _PP_RSEQ_N() 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
 
 #define _BINARY_PUSHER(TAG, A, B)                                \
