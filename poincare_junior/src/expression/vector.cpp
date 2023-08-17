1ba6959a100fb8571cf2f20633f48badb4d0ef16
MarcNumworks <marc.coudriau@numworks.com>
2023-08-17 14:07:31 +0200

    [pcj] Translate vector methods to PCJ

--- poincare_junior/src/expression/vector.cpp
+++ poincare_junior/src/expression/vector.cpp
@@ -1,70 +1,63 @@
 #include "vector.h"
 
+#include "k_tree.h"
+#include "simplification.h"
+
 namespace PoincareJ {
 
-Expression Matrix::norm(const ReductionContext &reductionContext) const {
+Tree* Vector::Norm(const Tree* v) {
   // Norm is defined on vectors only
-  assert(vectorType() != Array::VectorType::None);
-  Addition sum = Addition::Builder();
-  int childrenNumber = numberOfChildren();
-  for (int j = 0; j < childrenNumber; j++) {
-    Expression absValue = AbsoluteValue::Builder(
-        const_cast<Matrix *>(this)->childAtIndex(j).clone());
-    Expression squaredAbsValue = Power::Builder(absValue, Rational::Builder(2));
-    absValue.shallowReduce(reductionContext);
-    sum.addChildAtIndexInPlace(squaredAbsValue, sum.numberOfChildren(),
-                               sum.numberOfChildren());
-    squaredAbsValue.shallowReduce(reductionContext);
+  int childrenNumber = v->numberOfChildren();
+  Tree* result = KSqrt->cloneNode();
+  Tree* sum = SharedEditionPool->push<BlockType::Addition>(childrenNumber);
+  for (const Tree* child : v->children()) {
+    Tree* squaredAbsValue = KPow->cloneNode();
+    Tree* absValue = KAbs->cloneNode();
+    child->clone();
+    Simplification::ShallowSystematicReduce(absValue);
+    (2_e)->clone();
+    Simplification::ShallowSystematicReduce(squaredAbsValue);
   }
-  Expression result = SquareRoot::Builder(sum);
-  sum.shallowReduce(reductionContext);
+  Simplification::ShallowSystematicReduce(sum);
+  Simplification::ShallowSystematicReduce(result);
   return result;
 }
 
-Expression Matrix::dot(Matrix *b,
-                       const ReductionContext &reductionContext) const {
+Tree* Vector::Dot(const Tree* u, const Tree* v) {
   // Dot product is defined between two vectors of same size and type
-  assert(vectorType() != Array::VectorType::None &&
-         vectorType() == b->vectorType() &&
-         numberOfChildren() == b->numberOfChildren());
-  Addition sum = Addition::Builder();
-  int childrenNumber = numberOfChildren();
-  for (int j = 0; j < childrenNumber; j++) {
-    Expression product = Multiplication::Builder(
-        const_cast<Matrix *>(this)->childAtIndex(j).clone(),
-        const_cast<Matrix *>(b)->childAtIndex(j).clone());
-    sum.addChildAtIndexInPlace(product, sum.numberOfChildren(),
-                               sum.numberOfChildren());
-    product.shallowReduce(reductionContext);
+  int childrenNumber = v->numberOfChildren();
+  Tree* sum = SharedEditionPool->push<BlockType::Addition>(childrenNumber);
+  const Tree* childV = v->nextNode();
+  for (const Tree* childU : u->children()) {
+    Tree* product = KMult.node<2>->cloneNode();
+    childU->clone();
+    childV->clone();
+    childV = childV->nextTree();
+    Simplification::ShallowSystematicReduce(product);
   }
-  return std::move(sum);
+  Simplification::ShallowSystematicReduce(sum);
+  return sum;
 }
 
-Matrix Matrix::cross(Matrix *b,
-                     const ReductionContext &reductionContext) const {
+Tree* Vector::Cross(const Tree* u, const Tree* v) {
   // Cross product is defined between two vectors of size 3 and of same type.
-  assert(vectorType() != Array::VectorType::None &&
-         vectorType() == b->vectorType() && numberOfChildren() == 3 &&
-         b->numberOfChildren() == 3);
-  Matrix matrix = Matrix::Builder();
+  Tree* result = u->cloneNode();
   for (int j = 0; j < 3; j++) {
     int j1 = (j + 1) % 3;
     int j2 = (j + 2) % 3;
-    Expression a1b2 = Multiplication::Builder(
-        const_cast<Matrix *>(this)->childAtIndex(j1).clone(),
-        const_cast<Matrix *>(b)->childAtIndex(j2).clone());
-    Expression a2b1 = Multiplication::Builder(
-        const_cast<Matrix *>(this)->childAtIndex(j2).clone(),
-        const_cast<Matrix *>(b)->childAtIndex(j1).clone());
-    Expression difference = Subtraction::Builder(a1b2, a2b1);
-    a1b2.shallowReduce(reductionContext);
-    a2b1.shallowReduce(reductionContext);
-    matrix.addChildAtIndexInPlace(difference, matrix.numberOfChildren(),
-                                  matrix.numberOfChildren());
-    difference.shallowReduce(reductionContext);
+    Tree* difference = KAdd.node<2>->cloneNode();
+    Tree* a1b2 = KMult.node<2>->cloneNode();
+    u->childAtIndex(j1)->clone();
+    v->childAtIndex(j2)->clone();
+    Simplification::ShallowSystematicReduce(a1b2);
+    Tree* a2b1 = KMult.node<3>->cloneNode();
+    (-1_e)->clone();
+    u->childAtIndex(j2)->clone();
+    v->childAtIndex(j1)->clone();
+    Simplification::ShallowSystematicReduce(a2b1);
+    Simplification::ShallowSystematicReduce(difference);
   }
-  matrix.setDimensions(numberOfRows(), numberOfColumns());
-  return matrix;
+  return result;
 }
 
 }  // namespace PoincareJ
