99c779c280a72e217d9f6dc930987e8e720f6a15
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2024-01-17 11:14:40 +0100

    [pcj/test] Improve pcj_sign

--- poincare_junior/test/sign.cpp
+++ poincare_junior/test/sign.cpp
@@ -1,25 +1,22 @@
-#include <poincare_junior/src/expression/k_tree.h>
+#include <poincare_junior/src/expression/projection.h>
 #include <poincare_junior/src/expression/sign.h>
 #include <poincare_junior/src/expression/simplification.h>
 
 #include "helper.h"
 using namespace PoincareJ;
 
-bool check_sign(const char* input, Sign::Sign expectedSign,
-                bool shouldBeInteger = false) {
+void assert_sign(const char* input, Sign::Sign expectedSign,
+                 bool shouldBeInteger = false) {
   expectedSign.isInteger = shouldBeInteger;
   Tree* expression = TextToTree(input);
   Projection::DeepSystemProjection(expression);
-  bool result = Sign::GetSign(expression) == expectedSign;
+  QUIZ_ASSERT(Sign::GetSign(expression) == expectedSign);
   expression->removeTree();
-  return result;
 }
 
 QUIZ_CASE(pcj_sign) {
-  QUIZ_ASSERT(check_sign("2", Sign::PositiveInteger, true));
-  QUIZ_ASSERT(check_sign("2+π", Sign::Positive));
-  QUIZ_ASSERT(check_sign("2-4", Sign::Integer, true));
-  QUIZ_ASSERT(check_sign("3 * abs(cos(x)) * -2", Sign::NegativeOrNull));
-
-  QUIZ_ASSERT(SharedEditionPool->numberOfTrees() == 0);
+  assert_sign("2", Sign::PositiveInteger, true);
+  assert_sign("2+π", Sign::Positive);
+  assert_sign("2-4", Sign::Integer, true);
+  assert_sign("3 * abs(cos(x)) * -2", Sign::NegativeOrNull);
 }
