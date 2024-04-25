8857c32050d72141f669de9dd973d29c9200650b
MarcNumworks <marc.coudriau@numworks.com>
2024-04-25 16:29:48 +0200

    [poincare] Add k_layout.h to avoid exposing expressions superfluously

---
+++ poincare/include/poincare/k_layout.h
@@ -0,0 +1,7 @@
+#include <poincare/src/layout/k_tree.h>
+#include <poincare/src/memory/k_tree.h>
+// Placeholders KA, KB_s, KC_p, ...
+#include <poincare/src/memory/pattern_matching.h>
+
+// Expose KTrees namespace for shorter pattern code
+using namespace Poincare::Internal::KTrees;
