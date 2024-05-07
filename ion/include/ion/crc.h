7997221bf014b984ed1782aa5c9f63ccafdf5d00
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2024-05-07 17:57:20 +0200

    [ion/crc] Add TODO_PCJ marker

--- ion/include/ion/crc.h
+++ ion/include/ion/crc.h
@@ -13,8 +13,8 @@ uint32_t crc32DoubleWord(const uint32_t *data, size_t length);
 uint32_t crc32Word(const uint16_t *data, size_t length);
 uint32_t crc32Byte(const uint8_t *data, size_t length);
 
-/* FIXME This function only exists to avoid having to update epsilon-core,
- * remove it when appropriate. */
+/* FIXME TODO_PCJ This function only exists to avoid having to update
+ * epsilon-core, remove it when appropriate. */
 constexpr uint32_t crc32EatByte(uint32_t crc, uint8_t data) {
   return OMG::Memory::crc32EatByte(crc, data);
 }
