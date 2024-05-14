0a891edee26c38cd1dcba7e8898fff2f3eea450f
Pia Chancerel <pia.chancerel@numworks.com>
2024-05-14 16:26:16 +0200

    [poincare/test/old] poincare_simplification_random are ok

--- poincare/test/old/simplification.cpp
+++ poincare/test/old/simplification.cpp
@@ -1121,9 +1121,9 @@ QUIZ_CASE(poincare_simplification_random) {
   assert_parsed_expression_simplify_to("random()+random()",
                                        "random()+random()");
   assert_parsed_expression_simplify_to("random()-random()",
-                                       "-random()+random()");
+                                       "random()-random()");
   assert_parsed_expression_simplify_to("abs(random()-random())",
-                                       "abs(-random()+random())");
+                                       "abs(random()-random())");
   assert_parsed_expression_simplify_to("1/random()+1/3+1/4+1/random()",
                                        "7/12+1/random()+1/random()");
   assert_parsed_expression_simplify_to("random()×random()",
@@ -1131,9 +1131,9 @@ QUIZ_CASE(poincare_simplification_random) {
   assert_parsed_expression_simplify_to("random()/random()",
                                        "random()/random()");
   assert_parsed_expression_simplify_to("3^random()×3^random()",
-                                       "3^random()×3^random()");
+                                       "3^(random()+random())");
   assert_parsed_expression_simplify_to("random()×ln(2)×3+random()×ln(2)×5",
-                                       "5×ln(2)×random()+3×ln(2)×random()");
+                                       "3×ln(2)×random()+5×ln(2)×random()");
 }
 
 QUIZ_CASE(poincare_simplification_randint) {
