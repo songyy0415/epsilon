40fc238060fa8788f4ca507d3b8c43810f004595
MarcNumworks <marc.coudriau@numworks.com>
2024-05-17 16:24:20 +0200

    [shared] Comment unused variable breaking tests

--- apps/shared/global_context.cpp
+++ apps/shared/global_context.cpp
@@ -289,7 +289,7 @@ Ion::Storage::Record::ErrorStatus GlobalContext::setExpressionForFunction(
   ExpiringPointer<ContinuousFunction> f =
       GlobalContext::continuousFunctionStore->modelForRecord(recordToSet);
   // TODO: factorise with ContinuousFunction::setContent
-  bool wasCartesian = f->properties().isCartesian();
+  // TODO_PCJ: bool wasCartesian = f->properties().isCartesian();
   error = f->setExpressionContent(equation);
   if (error == Ion::Storage::Record::ErrorStatus::None) {
     // TODO_PCJ: f->updateModel(this, wasCartesian);
