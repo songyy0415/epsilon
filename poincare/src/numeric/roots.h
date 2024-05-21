439bc26867de6ccc7029e17b6496814e89b2e404
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2024-05-21 11:06:07 +0200

    [poincare/roots] Start implementing polynomial roots

---
+++ poincare/src/numeric/roots.h
@@ -0,0 +1,62 @@
+#ifndef POINCARE_NUMERIC_ROOTS_H
+#define POINCARE_NUMERIC_ROOTS_H
+
+#include <poincare/src/memory/tree.h>
+
+namespace Poincare::Internal {
+
+class Roots {
+ public:
+  // Return the only root.
+  static Tree* Linear(const Tree* a, const Tree* b);
+  // Return the only root or a list of two roots
+  static Tree* Quadratic(const Tree* a, const Tree* b, const Tree* c);
+#if 0
+  static int LinearPolynomialRoots(OExpression a, OExpression b,
+                                   OExpression* root,
+                                   ReductionContext reductionContext,
+                                   bool beautifyRoots = true);
+
+  static int QuadraticPolynomialRoots(OExpression a, OExpression b,
+                                      OExpression c, OExpression* root1,
+                                      OExpression* root2, OExpression* delta,
+                                      ReductionContext reductionContext,
+                                      bool* approximateSolutions = nullptr,
+                                      bool beautifyRoots = true);
+
+  static int CubicPolynomialRoots(OExpression a, OExpression b, OExpression c,
+                                  OExpression d, OExpression* root1,
+                                  OExpression* root2, OExpression* root3,
+                                  OExpression* delta,
+                                  ReductionContext reductionContext,
+                                  bool* approximateSolutions = nullptr,
+                                  bool beautifyRoots = true);
+
+ private:
+  constexpr static int k_maxNumberOfNodesBeforeApproximatingDelta = 16;
+  static OExpression ReducePolynomial(const OExpression* coefficients,
+                                      int degree, OExpression parameter,
+                                      const ReductionContext& reductionContext);
+  static Rational ReduceRationalPolynomial(const Rational* coefficients,
+                                           int degree, Rational parameter);
+  static bool IsRoot(const OExpression* coefficients, int degree,
+                     OExpression root,
+                     const ReductionContext& reductionContext) {
+    return ReducePolynomial(coefficients, degree, root, reductionContext)
+               .isNull(reductionContext.context()) == OMG::Troolean::True;
+  }
+  static OExpression RationalRootSearch(
+      const OExpression* coefficients, int degree,
+      const ReductionContext& reductionContext);
+  static OExpression SumRootSearch(const OExpression* coefficients, int degree,
+                                   int relevantCoefficient,
+                                   const ReductionContext& reductionContext);
+  static OExpression CardanoNumber(OExpression delta0, OExpression delta1,
+                                   bool* approximate,
+                                   const ReductionContext& reductionContext);
+#endif
+};
+
+}  // namespace Poincare::Internal
+
+#endif
---
+++ poincare/test/roots.cpp
@@ -0,0 +1,52 @@
+#include <poincare/src/expression/simplification.h>
+#include <poincare/src/numeric/roots.h>
+
+#include "helper.h"
+
+using namespace Poincare::Internal;
+
+void assert_roots_are(const char* coefficients, const char* expectedRoots) {
+  ProjectionContext projCtx = {.m_complexFormat = ComplexFormat::Cartesian};
+  Tree* coeff = parse(coefficients);
+  Simplification::ToSystem(coeff, &projCtx);
+  Simplification::SimplifySystem(coeff, true);
+  int numberOfCoefficients = coeff->numberOfChildren();
+  Tree* result = nullptr;
+  switch (numberOfCoefficients) {
+    case 2:
+      result = Roots::Linear(coeff->child(0), coeff->child(1));
+      break;
+    case 3:
+      result =
+          Roots::Quadratic(coeff->child(0), coeff->child(1), coeff->child(2));
+      break;
+    default:
+      // Not handled
+      quiz_assert(false);
+  }
+  // TODO: Advanced reduction is needed to simplify roots of rationals
+  Simplification::SimplifySystem(result, true);
+  Tree* expected = parse(expectedRoots);
+  Simplification::ToSystem(expected, &projCtx);
+  Simplification::SimplifySystem(expected, false);
+  assert_trees_are_equal(expected, result);
+  expected->removeTree();
+  result->removeTree();
+  coeff->removeTree();
+}
+
+QUIZ_CASE(pcj_roots) {
+  assert_roots_are("{6, 2}", "-1/3");
+  assert_roots_are("{1, -1}", "1");
+  assert_roots_are("{2x+z, 4y-1}", "(1-4y)/(2x+z)");
+  assert_roots_are("{1, -2, 1}", "1");
+  assert_roots_are("{π, -2π, π}", "1");
+  assert_roots_are("{1, -1, -6}", "{-2, 3}");
+  // TODO: Results should be simpler but ADVANCED_MAX_DEPTH is too small
+  // {-1×i,i}
+  assert_roots_are("{1, 0, 1}", "{-1/2×exp(1/2×ln(-4)),exp(1/2×π×i)}");
+  // {-1/2×(-1-1×x+exp(1/2×ln(1-2×x+x^2))),1/2×(1+x+exp(1/2×ln(1-2×x+x^2)))}
+  assert_roots_are("{1,-x-1,x}",
+                   "{-1/2×(-1-1×x+exp(1/2×ln(1-2×x+x^2))),1/"
+                   "2×(1+x+exp(1/2×ln(1-2×x+x^2)))}");
+}
