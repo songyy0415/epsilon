e842c94ff160d8f44569f4e54428315e0e20200e
MarcNumworks <marc.coudriau@numworks.com>
2024-06-10 18:09:36 +0200

    refactor(poincare): import and adapt methods from LinearRegressionStore

--- poincare/src/regression/dataset_adapter.h
+++ poincare/src/regression/dataset_adapter.h
@@ -1,10 +1,13 @@
 #ifndef POINCARE_REGRESSION_DATASET_ADAPTER_H
 #define POINCARE_REGRESSION_DATASET_ADAPTER_H
 
+#include <apps/shared/double_pair_store.h>
 #include <poincare/src/numeric/statistics_dataset.h>
 #include <poincare/src/numeric/statistics_dataset_column.h>
 
 #include "regression.h"
+
+using CalculationOptions = Shared::DoublePairStore::CalculationOptions;
 
 namespace Poincare::Regression {
 
@@ -25,12 +28,65 @@ class DatasetColumnSeriesAdapter : public Internal::DatasetColumn<double> {
   int m_column;
 };
 
-class DatasetSeriesAdapter : public StatisticsDataset<double> {
+class OneColumn : public Internal::StatisticsDataset<double> {
+ public:
+  OneColumn(const Series* series, int column)
+      : StatisticsDataset(&m_columnAdapter), m_columnAdapter(series, column) {}
+
+ private:
+  const DatasetColumnSeriesAdapter m_columnAdapter;
+};
+
+class DatasetSeriesAdapter : public Internal::StatisticsDataset<double> {
  public:
   DatasetSeriesAdapter(const Series* series)
       : StatisticsDataset(&m_xAdapter, &m_yAdapter),
         m_xAdapter(series, 0),
         m_yAdapter(series, 1) {}
+
+  double columnProductSum(CalculationOptions options) const {
+    double result = 0;
+    int numberOfPairs = numberOfPairsOfSeries();
+    for (int k = 0; k < numberOfPairs; k++) {
+      double value0 = options.transformValue(m_xAdapter.valueAtIndex(k), 0);
+      double value1 = options.transformValue(m_yAdapter.valueAtIndex(k), 1);
+      result += value0 * value1;
+    }
+    return result;
+  }
+
+  double covariance(CalculationOptions options) const {
+    double mean0 = meanOfColumn(0, options);
+    double mean1 = meanOfColumn(1, options);
+    return columnProductSum(options) / numberOfPairsOfSeries() - mean0 * mean1;
+  }
+
+  double numberOfPairsOfSeries() const { return m_xAdapter.length(); }
+
+  double slope(CalculationOptions options = {}) const {
+    return covariance(options) / varianceOfColumn(0, options);
+  }
+
+  double yIntercept(CalculationOptions options = {}) const {
+    double meanOfX = meanOfColumn(0, options);
+    double meanOfY = meanOfColumn(1, options);
+    return meanOfY - slope(options) * meanOfX;
+  }
+
+  double meanOfColumn(int i, CalculationOptions options) const {
+    return createDatasetFromColumn(i, options).mean();
+  }
+
+  double varianceOfColumn(int i, CalculationOptions options) const {
+    return createDatasetFromColumn(i, options).variance();
+  }
+
+  Poincare::StatisticsDataset<double> createDatasetFromColumn(
+      int i, CalculationOptions options) const {
+    return Poincare::StatisticsDataset<double>(
+        i == 0 ? &m_xAdapter : &m_yAdapter, options.lnOfValue(i),
+        options.oppositeOfValue(i));
+  }
 
  private:
   const DatasetColumnSeriesAdapter m_xAdapter;
