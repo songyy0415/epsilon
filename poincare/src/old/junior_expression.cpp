159103e6b88c49b10fbf027566284605e7759bbb
MarcNumworks <marc.coudriau@numworks.com>
2024-08-14 16:43:07 +0200

    docs: update comment

--- poincare/src/old/junior_expression.cpp
+++ poincare/src/old/junior_expression.cpp
@@ -519,7 +519,7 @@ void UserExpression::cloneAndSimplifyAndApproximate(
     Tree* a = e->cloneTree();
     /* We are using ApproximateAndReplaceEveryScalar to approximate expressions
      * with symbols such as π*x → 3.14*x.  We could use
-     * Approximation::RootTreeToTree if CAS is not enabled. */
+     * Approximation::RootTreeToTree when CAS is not enabled. */
     Approximation::ApproximateAndReplaceEveryScalar(a, context);
     *approximatedExpression = Builder(a);
   }
