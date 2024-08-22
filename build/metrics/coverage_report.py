83701b2733545fbb282ca3ee7f2795e5638d4421
LoreneNumworks <lorene.imperor@numworks.com>
2024-08-22 11:30:17 +0200

    feature(ci/coverage): parse and format differential report

--- build/metrics/coverage_report.py
+++ build/metrics/coverage_report.py
@@ -1,23 +1,3 @@
-# ### Coverage report
-
-# #### Coverage rate for commit bace878
-
-# |               	        |   **Lines**   	        | **Functions** 	|
-# |---------------	|:-------------:	|:-------------:	|
-# | Coverage rate 	|     65.6%     	        |     73.2%     	        |
-# | Hits / Total  	        | 55059 / 83952 	| 12592 / 17209 	|
-
-# #### Differences with base branch (bace878)
-
-# |               	                              |   **Lines**   	        | **Functions** 	|
-# |---------------	                      |:-------------:	|:-------------:	|
-# | ⚠️ Uncovered New Code 	      |     10      	        |     3       	        |
-# | ⚠️ Lost Baseline Coverage        |     311               |     86       	        |
-# | ✅ Gained New Coverage          |     6      	        |     2       	        |
-# | ✅ Gained Baseline Coverage   |     4      	        |     2       	        |
-
-# Download full report: https://github.com/numworks/epsilon-internal/actions/runs/10490330078/artifacts/1837391704
-
 import argparse
 import parse
 from dataclasses import dataclass
@@ -33,42 +13,75 @@ class FullSummary:
   lines: Summary
   functions: Summary
 
-@dataclass
-class Diff:
-  UNC: int = 0
-  LBC: int = 0
-  GNC: int = 0
-  GBC: int = 0
+# We do not display "UBC" (uncovered base coverage)  and "CBC" (covered base coverage), both of which are always present in
+# the differential report, because they are redundant with the coverage summary (hits / total).
+# A detailed description of the categories can be found here: https://manpages.debian.org/unstable/lcov/genhtml.1.en.html#
+categories_to_display = {'UNC': "⚠️ Uncovered New Code",
+                         'UIC': "⚠️ Uncovered Included Code",
+                         'LBC': "⚠️ Lost Baseline Coverage",
+                         'GNC': "✅ Gained New Coverage",
+                         'GIC': "✅ Gained Included Coverage",
+                         'GBC': "✅ Gained Baseline Coverage",
+                         'EUB': "Excluded Uncovered Baseline",
+                         'ECB': "Excluded Covered Baseline",
+                         'DUB': "Deleted Uncovered Baseline",
+                         'DCB': "Deleted Covered Baseline"}
 
-@dataclass
 class FullDiff:
-  lines: Diff
-  functions: Diff
+  def __init__(self):
+    self.line_counts = {'UNC': 0, 'UIC': 0, 'LBC': 0, 'GNC': 0, 'GIC': 0, 'GBC': 0, 'EUB': 0, 'ECB': 0, 'DUB': 0, 'DCB': 0}
+    self.function_counts = {'UNC': 0, 'UIC': 0, 'LBC': 0, 'GNC': 0, 'GIC': 0, 'GBC': 0, 'EUB': 0, 'ECB': 0, 'DUB': 0, 'DCB': 0}
 
 def format_summary(summary: FullSummary):
-  return f"""|    |   **Lines**                                   | **Functions**                                         |
-|---------------  |:-------------:                                |:-------------:                                        |
-| Coverage rate   |          {summary.lines.percentage} %         |            {summary.functions.percentage} %           |
-| Hits / Total    | {summary.lines.hits} / {summary.lines.total}  | {summary.functions.hits} / {summary.functions.total}  |"""
+  return f"""
+  |                     |   **Lines**                                   | **Functions**                                         |
+  |---------------------|:---------------------------------------------:|:-----------------------------------------------------:|
+  |    Coverage rate    |          {summary.lines.percentage} %         |            {summary.functions.percentage} %           |
+  |    Hits / Total     | {summary.lines.hits} / {summary.lines.total}  | {summary.functions.hits} / {summary.functions.total}  |"""
 
 
-def format_diff(differential: FullDiff):
-  return "diff"
+def format_diff(diff: FullDiff):
+  output = """
+  |                     |   **Lines**                                   | **Functions**                                         |
+  |---------------------|:---------------------------------------------:|:-----------------------------------------------------:|"""
+
+  for category, description in categories_to_display.items():
+    if diff.line_counts[category] > 0 or diff.function_counts[category] > 0:
+      output += f"""
+  |     {description}   |        {diff.line_counts[category]}           |             {diff.function_counts[category]}          |"""
+  return output
 
 def parse_summary(genhtml_txt: str):
   with open(genhtml_txt, "r") as f:
     lines = f.readlines()
   for line in lines:
     if line.lstrip().startswith("lines"):
-      result = parse.search(": {}% ({} of {} lines)", line)
-      line_summary = Summary(result[0], result[1], result[2])
+      percentage, hits, total = parse.search(": {}% ({} of {} lines)", line)
+      line_summary = Summary(percentage, hits, total)
     if line.lstrip().startswith("functions"):
-      result = parse.search(": {}% ({} of {} functions)", line)
-      function_summary = Summary(result[0], result[1], result[2])
+      percentage, hits, total = parse.search(": {}% ({} of {} functions)", line)
+      function_summary = Summary(percentage, hits, total)
   return FullSummary(line_summary, function_summary)
 
-def parse_diff(genhtml_txt):
-  return FullDiff(Diff(UNC=3, GNC=10), Diff(GBC=2, LBC=24))
+def parse_diff(genhtml_txt: str):
+  with open(genhtml_txt, "r") as f:
+    lines = f.readlines()
+    diff = FullDiff()
+    editing = None
+    for line in lines:
+      if line.lstrip().startswith("lines"):
+        editing = diff.line_counts
+        continue
+      if line.lstrip().startswith("functions"):
+        editing = diff.function_counts
+        continue
+      if line.lstrip().startswith("Message"):
+        break
+      if editing:
+        category, count = parse.parse("{}...: {}", line.lstrip())
+        if (category in categories_to_display):
+          editing[category] = int(count)
+  return diff
 
 
 if __name__ == "__main__":
