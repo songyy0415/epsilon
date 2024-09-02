4d93067653ffb228b6e0a4ea90505b2057fad546
Pia Chancerel <pia.chancerel@numworks.com>
2024-09-02 11:24:12 +0200

    test(poincare): fix pcj_roots
    expectedRoots can't be ProjectAndReduce

--- poincare/test/roots.cpp
+++ poincare/test/roots.cpp
@@ -7,42 +7,40 @@ using namespace Poincare::Internal;
 
 void assert_roots_are(const char* coefficients, const char* expectedRoots) {
   ProjectionContext projCtx = {.m_complexFormat = ComplexFormat::Cartesian};
-  Tree* coeff = parse(coefficients);
-  Simplification::ProjectAndReduce(coeff, &projCtx, true);
-  int numberOfCoefficients = coeff->numberOfChildren();
-  Tree* result = nullptr;
-  switch (numberOfCoefficients) {
-    case 2:
-      result = Roots::Linear(coeff->child(0), coeff->child(1));
-      break;
-    case 3:
-      result =
-          Roots::Quadratic(coeff->child(0), coeff->child(1), coeff->child(2));
-      break;
-    default:
-      // Not handled
-      quiz_assert(false);
-  }
-  // TODO: Advanced reduction is needed to simplify roots of rationals
-  Simplification::ReduceSystem(result, true);
-  Tree* expected = parse(expectedRoots);
-  Simplification::ProjectAndReduce(expected, &projCtx, false);
-  assert_trees_are_equal(expected, result);
-  expected->removeTree();
-  result->removeTree();
-  coeff->removeTree();
+  process_tree_and_compare(
+      coefficients, expectedRoots,
+      [](Tree* tree, ProjectionContext projCtx) {
+        Simplification::ProjectAndReduce(tree, &projCtx, true);
+        int numberOfCoefficients = tree->numberOfChildren();
+        switch (numberOfCoefficients) {
+          case 2:
+            tree->moveTreeOverTree(
+                Roots::Linear(tree->child(0), tree->child(1)));
+            break;
+          case 3:
+            tree->moveTreeOverTree(Roots::Quadratic(
+                tree->child(0), tree->child(1), tree->child(2)));
+            break;
+          default:
+            // Not handled
+            quiz_assert(false);
+        }
+        // TODO: Advanced reduction is needed to simplify roots of rationals
+        Simplification::ReduceSystem(tree, true);
+        Simplification::BeautifyReduced(tree, &projCtx);
+      },
+      projCtx);
 }
 
 QUIZ_CASE(pcj_roots) {
   assert_roots_are("{6, 2}", "-1/3");
   assert_roots_are("{1, -1}", "1");
-  assert_roots_are("{2x+z, 4y-1}", "(1-4y)/(2x+z)");
+  assert_roots_are("{2x+z, 4y-1}", "(-4×y+1)/(2×x+z)");
   assert_roots_are("{1, -2, 1}", "1");
   assert_roots_are("{π, -2π, π}", "1");
-  assert_roots_are("{1, -1, -6}", "{3, -2}");
+  assert_roots_are("{1, -1, -6}", "{3,-2}");
   assert_roots_are("{1,-x-1,x}",
-                   "{1/2×(1+x+exp(1/2×ln(1-2×x+x^2))), "
-                   "-1/2×(-1-1×x+exp(1/2×ln(1-2×x+x^2)))}");
+                   "{(x+1+√(x^2-2×x+1))/2,-(-x-1+√(x^2-2×x+1))/2}");
   // TODO: Should simplify to {-1×i,i}
-  assert_roots_are("{1, 0, 1}", "{1/2×exp(1/2×ln(-4)),-1×i}");
+  assert_roots_are("{1, 0, 1}", "{√(-4)/2,-i}");
 }
