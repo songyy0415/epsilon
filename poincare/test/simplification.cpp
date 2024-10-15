475e5152d2c19146be4e44d360d6aa0af627c1af
PaulineNumWorks <pauline.garelli@numworks.com>
2024-10-15 15:46:21 +0200

    test: restore altered test and add reduction of arccosh

--- poincare/test/simplification.cpp
+++ poincare/test/simplification.cpp
@@ -509,8 +509,9 @@ QUIZ_CASE(pcj_simplification_hyperbolic_trigonometry) {
   // TODO: Should simplify to 0
   simplifies_to("((1+tanh(x)^2)*tanh(2x)/2)-tanh(x)",
                 "tan(2×x×i)×i×(sin(x×i)^2/(2×cos(x×i)^2)-1/2)+tan(x×i)×i");
+  simplifies_to("arcosh(5)", "arcosh(5)", cartesianCtx);
+  simplifies_to("arcosh(5)-ln(5+sqrt(24))", "0", cartesianCtx);
   simplifies_to("arcosh(cosh(x))", "abs(x)", cartesianCtx);
-  simplifies_to("arsinh(sinh(x))", "x", cartesianCtx);
   simplifies_to("arsinh(sinh(x))", "x", cartesianCtx);
   simplifies_to("artanh(tanh(x))", "x", cartesianCtx);
   simplifies_to("cosh(arcosh(x))", "x", cartesianCtx);
