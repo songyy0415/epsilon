9db3890cbebb3763cbe93e4bcad09d30c33cd512
LoreneNumworks <lorene.imperor@numworks.com>
2024-11-22 15:34:27 +0100

    refactor(statistics/histogram): remove old implementation files

--- escher/include/escher/solid_color_cell.h
+++ escher/include/escher/solid_color_cell.h
@@ -8,20 +8,17 @@ namespace Escher {
 
 /* Simple empty HighlightCell with a solid color. */
 
-// TODO: restore the original file when histogram cells are done
 class SolidColorCell : public HighlightCell {
  public:
   SolidColorCell(KDColor color = KDColorWhite)
       : HighlightCell(), m_color(color) {}
   void drawRect(KDContext* ctx, KDRect rect) const override {
-    ctx->fillRect(rect, isHighlighted() ? m_highlightColor : m_color);
+    ctx->fillRect(rect, m_color);
   }
   void setColor(KDColor color) { m_color = color; }
-  void setHighlightColor(KDColor color) { m_highlightColor = color; }
 
  private:
   KDColor m_color;
-  KDColor m_highlightColor = KDColorWhite;
 };
 
 }  // namespace Escher
