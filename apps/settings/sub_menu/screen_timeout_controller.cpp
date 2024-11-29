ee250a8a4bdf7f7448de1da0609772f0c5336a40
LoreneNumworks <lorene.imperor@numworks.com>
2024-11-29 15:39:08 +0100

    feature(apps/settings): only display the battery warning if the timeout is more than the current one

--- apps/settings/sub_menu/screen_timeout_controller.cpp
+++ apps/settings/sub_menu/screen_timeout_controller.cpp
@@ -21,15 +21,15 @@ ScreenTimeoutController::ScreenTimeoutController(
 
 bool ScreenTimeoutController::handleEvent(Ion::Events::Event event) {
   if (event == Ion::Events::OK || event == Ion::Events::EXE) {
-    if (static_cast<DimmingTimeLabel>(selectedRow()) ==
-        DimmingTimeLabel::ThirtySeconds) {
-      /* No warning pop-up about the battery lifetime if the selected dimming
-       * time is 30 seconds only */
-      setDimmingTimePreference(selectedRow());
-      stackController()->pop();
+    if (toDimmingTime(static_cast<DimmingTimeLabel>(selectedRow())) >
+        GlobalPreferences::SharedGlobalPreferences()->dimmingTime()) {
+      /* If the newly selected dimming time is more than the current setting,
+       * display a warning pop-up about the reduced battery lifetime. */
+      m_warningPopUpController.presentModally();
       return true;
     }
-    m_warningPopUpController.presentModally();
+    setDimmingTimePreference(selectedRow());
+    stackController()->pop();
     return true;
   }
   return GenericSubController::handleEvent(event);
