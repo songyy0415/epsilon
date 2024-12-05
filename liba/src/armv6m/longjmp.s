d87b89305a079b442cd602952b629c134d5b2304
MarcNumworks <marc.coudriau@numworks.com>
2024-12-05 17:09:02 +0100

    feat(liba): implement setjmp/longjmp for armv6m

--- liba/src/armv6m/longjmp.s
+++ liba/src/armv6m/longjmp.s
@@ -7,6 +7,22 @@
 longjmp:
   /* Restore all the regsiters to get back in the original state (whenever the
      matching setjmp was called. */
-  bkpt #0 // TODO cf armv7m implementation
+  ldmia r0!, { r4-r7 }
+  ldmia r0!, { r2-r3 }
+  mov r8, r2
+  mov r9, r3
+  ldmia r0!, { r2-r3 }
+  mov r10, r2
+  mov fp, r3
+  ldmia r0!, { r2-r3 }
+  mov sp, r2
+  mov lr, r3
+  /* Put the return value in the integer result register, but return 1 if it is
+     in fact zero. */
+  movs r0, r1
+  cmp r0, #0
+  bne _ret
+  movs r0, #1
+_ret:
   bx lr
 .type longjmp, function
--- liba/src/armv6m/setjmp.s
+++ liba/src/armv6m/setjmp.s
@@ -6,6 +6,19 @@
 .global setjmp
 setjmp:
   /* Save all the registers into the jump buffer */
-  bkpt #0 // TODO cf armv7m implementation
+  mov ip, sp
+  // storing r4-r9, r10=sl, fp, sp, lr
+  stmia r0!, { r4-r7 }
+  // since we cannot use stmia with regs after r7 we do it in three steps
+  mov r1, r8
+  mov r2, r9
+  mov r3, r10
+  stmia r0!, { r1-r3 }
+  mov r1, fp
+  mov r2, sp
+  mov r3, lr
+  stmia r0!, { r1-r3 }
+  /* And then return 0 */
+  movs r0, #0
   bx lr
 .type setjmp, function
