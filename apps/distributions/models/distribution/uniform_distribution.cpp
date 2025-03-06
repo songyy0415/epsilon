8e32b0639e42c813990439aec6e17467f79c7ef8
Félix Richart <felix.richart@numworks.com>
2025-03-06 16:15:31 +0100

    docs(apps/distribution): comment the specialization of evaluateAtAbscissa for UniformDistribution

--- apps/distributions/models/distribution/uniform_distribution.cpp
+++ apps/distributions/models/distribution/uniform_distribution.cpp
@@ -14,6 +14,8 @@ namespace Distributions {
 float UniformDistribution::evaluateAtAbscissa(float t) const {
   float parameter1 = m_parameters[0];
   float parameter2 = m_parameters[1];
+  /* This is a hack to have better rendering of the distributions when the
+   * parameters are very close */
   if (parameter2 - parameter1 < FLT_EPSILON) {
     if (parameter1 - k_diracWidth <= t && t <= parameter2 + k_diracWidth) {
       return 2.0f * k_diracMaximum;
