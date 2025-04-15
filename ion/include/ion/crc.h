585735d16660146bdfae9c79602decca4e8d12ab
MarcNumworks <marc.coudriau@numworks.com>
2025-04-15 14:09:46 +0200

    refactor(ion): remove Ion::crc32EatByte

--- ion/include/ion/crc.h
+++ ion/include/ion/crc.h
@@ -13,12 +13,6 @@ uint32_t crc32DoubleWord(const uint32_t* data, size_t length);
 uint32_t crc32Word(const uint16_t* data, size_t length);
 uint32_t crc32Byte(const uint8_t* data, size_t length);
 
-/* FIXME TODO_PCJ This function only exists to avoid having to update
- * core, remove it when appropriate. */
-constexpr uint32_t crc32EatByte(uint32_t crc, uint8_t data) {
-  return OMG::Memory::crc32EatByte(crc, data);
-}
-
 }  // namespace Ion
 
 #endif
