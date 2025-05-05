93c9d3b337468df18432c88be2b76ebb68c239b6
LoreneNumworks <lorene.imperor@numworks.com>
2025-05-05 11:25:02 +0200

    refactor(power_like): optimize code

--- poincare/src/expression/power_like.cpp
+++ poincare/src/expression/power_like.cpp
@@ -43,25 +43,18 @@ bool PowerLike::ExpandRationalPower(Tree* e, const Tree* base,
   }
   assert(PowerLike::Exponent(e, true)->treeIsIdenticalTo(power));
   assert(PowerLike::Base(e, true)->treeIsIdenticalTo(base));
-  // e = x^(p/q) is expanded into x^n * x^(r/q) where r is the remainder of p/q
-  TreeRef p = Rational::Numerator(power).pushOnTreeStack();
-  TreeRef q = Rational::Denominator(power).pushOnTreeStack();
-  assert(!q->isOne());
-  TreeRef r =
-      IntegerHandler::Remainder(Integer::Handler(p), Integer::Handler(q));
-  // n = (p-r)/q
-  TreeRef n = PatternMatching::CreateSimplify(
-      KMult(KAdd(KA, KMult(-1_e, KC)), KPow(KB, -1_e)),
-      {.KA = p, .KB = q, .KC = r});
-  assert(n->isInteger());
+  /* e = x^(p/q) is expanded into x^n * x^(r/q) using euclidean division:
+   * p = n*q + r => p/q = n + r/q */
+  DivisionResult division = IntegerHandler::Division(
+      Rational::Numerator(power), Rational::Denominator(power));
+  Tree* q = Rational::Denominator(power).pushOnTreeStack();
   // result = base^n * exp(r/q * ln(base))
   TreeRef result = PatternMatching::CreateSimplify(
       KMult(KPow(KA, KB), KExp(KMult(KC, KPow(KD, -1_e), KLn(KA)))),
-      {.KA = base, .KB = n, .KC = r, .KD = q});
-  n->removeTree();
-  r->removeTree();
+      {.KA = base, .KB = division.quotient, .KC = division.remainder, .KD = q});
   q->removeTree();
-  p->removeTree();
+  division.remainder->removeTree();
+  division.quotient->removeTree();
   e->moveTreeOverTree(result);
   return true;
 }
